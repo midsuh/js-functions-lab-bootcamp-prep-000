7ff8f6f7706770ab5a1d0d6e9817490b62d581ad
irusaf@yahoo.com <irusaf@yahoo.com>
2018-09-01 20:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(holiday, days) {
+function holidayCountdown(days, holiday) {
   
   return `It's ${days} days until ${holiday}!`;
 }
